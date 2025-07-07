3658e54fe428e7320ff2ab77776b3c1285b1dcec
moriT958 <morit958@icloud.com>
2025-07-07 19:36:56 +0900

    split generator file

---
+++ patterns/generator.go
@@ -0,0 +1,18 @@
+package patterns
+
+import "context"
+
+func Generator(ctx context.Context, nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for _, n := range nums {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- n:
+			}
+		}
+	}()
+	return out
+}
--- patterns/pipeline.go
+++ patterns/pipeline.go
@@ -4,22 +4,6 @@ import (
 	"context"
 )
 
-func Generator(ctx context.Context, nums ...int) <-chan int {
-	out := make(chan int)
-	go func() {
-		defer close(out)
-		for _, n := range nums {
-			select {
-			case <-ctx.Done():
-				// cancelコンテキストを受信したら終了
-				return
-			case out <- n:
-			}
-		}
-	}()
-	return out
-}
-
 func Double(ctx context.Context, in <-chan int) <-chan int {
 	out := make(chan int)
 	go func() {
